8822e648f42bc4223228a9dcc02844a694c71857
Kevin Lawver <kevin@lawver.net>
2011-10-15 17:50:22 -0400

    Initial checkin.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hackaphoto::Application.config.secret_token = '040dcfb34ed53486dc5b6a2f049553d81d949bbe46eab3b1d69912b77b60594cff64f44790a9bc93de7a9642e177984391afd32fcd4a2ceffdc56d9098486aa5'
